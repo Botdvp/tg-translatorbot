508ed9eed49d63e49343fd824360a0f009b3247a
Botdvp <91675155+Botdvp@users.noreply.github.com>
2022-06-16 17:08:21 +0300

    config.py

--- Bot/config.py
+++ Bot/config.py
@@ -4,7 +4,7 @@ api_id = getenv ("API_ID")
 api_hash = getenv ("API_HASH")
 bot_token = getenv ("BOT_TOKEN")
 admin_id = getenv ("ADMIN_ID")
-databse_uri = getenv ("DATABASE_URI")
+database_uri = getenv ("DATABASE_URI")
 lang_list = {
 #callback # text
     "en": "english",
